5e407e004ab0d1267c2a81e9b5874225c1503990
Ivanlvanov <donawqa@gmail.com>
2026-01-03 23:49:46 +0100

    Added OrderService.cs

---
+++ System.Tests.Unit/OrderService.cs
@@ -0,0 +1,26 @@
+﻿using System.Domain.Services;
+using System.Domain.Models;
+using Moq;
+
+namespace System.Tests.Unit;
+
+[TestFixture]
+public class OrderServiceTests
+{
+    [Test]
+    public void ExecuteOrder_ExecutedOrder_SendsNotification()
+    {
+        var mockNotification = new Mock<INotificationService>();
+        var service = new OrderService(mockNotification.Object);
+
+        var trader = new Trader("John", 10000);
+        var stock = new Stock("AAPL", 100);
+        var order = new Order(stock, 10, OrderType.Buy);
+
+        service.ExecuteOrder(order, trader);
+
+        mockNotification.Verify(
+            n => n.NotifyTrader(It.IsAny<string>()),
+            Times.Once);
+    }
+}
